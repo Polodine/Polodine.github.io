d1cf4f24d444f12aaf9e1a50cb05fedf754a7830
Polodine <bamzurov72@gmail.com>
2016-05-17 17:23:57 +0300

    s

--- 1.js
+++ 1.js
@@ -54,12 +54,6 @@
             if (el.nextElementSibling){
               el.nextElementSibling.style.marginTop = originalRect.height + "px";
             }
-            psevdo.innerHTML += '.';
-            b++;
-            if (b == 100){
-              psevdo.innerHTML += '<br>';
-              b = 0;
-            }
           } else  {
             for (key in originalStyles) {
               el.style[key] = originalStyles[key];
@@ -90,7 +84,14 @@
             }
             if (el.nextElementSibling && el.nextElementSibling.style.marginTop)
               el.nextElementSibling.style.marginTop = 0;
+                        psevdo.innerHTML += '.';
+            b++;
+            if (b == 100){
+              psevdo.innerHTML += '<br>';
+              b = 0;
+            }
           }
+
           a && a(event)
         }
       }
