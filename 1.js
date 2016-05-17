a75f2a1df16c1635fab89d6dad61af07cdefadaa
Polodine <bamzurov72@gmail.com>
2016-05-17 07:04:23 +0300

    Bug?

--- 1.js
+++ 1.js
@@ -5,40 +5,54 @@
 }('sticky', function() {
 
   return function sticky(el, top) {
-
-    var requiredOriginalStyles = ['position', 'left', 'top', 'z-index'];
-
-    var requiredTop = top || 0;
-    var originalRect = calcRect(el);
-    var styles = {
-      position: 'fixed',
-      left: originalRect.left + 'px',
-      top: requiredTop + 'px',
-      'z-index': 9999
-    }
-    var originalStyles = {}
-    requiredOriginalStyles.forEach(function(key) {
-      originalStyles[key] = el.style[key];
-    });
-
-    // var a;
-
-    // if (a) {
-    //   a = window.onscroll;
-    // }
-    
-    setInterval(function(){
-      if (getWindowScroll().top > originalRect.top - requiredTop) {
-        for (key in styles) {
-          el.style[key] = styles[key];
-        }
-      } else {
-        for (key in originalStyles) {
-          el.style[key] = originalStyles[key];
-        }
+    var userAgent = window.navigator.userAgent;
+    if ((userAgent.toLowerCase().indexOf('android') > -1 )
+    && (userAgent.toLowerCase().indexOf('firefox') > -1)){
+      alert('andro');
+      window.onload = function(){
+        stickyIn();
       }
-      // a && a(event)
-    }, 20)
+    }
+
+    else {
+      alert('not');
+      stickyIn();
+    }
+    function stickyIn(){
+      var requiredOriginalStyles = ['position', 'left', 'top', 'z-index'];
+
+      var requiredTop = top || 0;
+      var originalRect = calcRect(el);
+      var styles = {
+        position: 'fixed',
+        left: originalRect.left + 'px',
+        top: requiredTop + 'px',
+        'z-index': 9999
+      }
+      var originalStyles = {}
+      requiredOriginalStyles.forEach(function(key) {
+        originalStyles[key] = el.style[key];
+      });
+
+      // var a;
+
+      // if (a) {
+      //   a = window.onscroll;
+      // }
+      
+      setInterval(function(){
+        if (getWindowScroll().top > originalRect.top - requiredTop) {
+          for (key in styles) {
+            el.style[key] = styles[key];
+          }
+        } else {
+          for (key in originalStyles) {
+            el.style[key] = originalStyles[key];
+          }
+        }
+        // a && a(event)
+      }, 20)
+    }
 
   }
 
