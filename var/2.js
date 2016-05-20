b4d9f426d8858e350573f89a02b5a39c46df3f3a
Polodine <bamzurov72@gmail.com>
2016-05-20 08:53:03 +0300

    fwa

--- var/2.js
+++ var/2.js
@@ -1,22 +1,22 @@
 $(function(){
-  var destroyer = 0;
-  var userAgent = window.navigator.userAgent;
-  var topElement = document.getElementById('bar2').getBoundingClientRect().top + window.pageYOffset;
-  if (((userAgent.toLowerCase().indexOf('android') > -1 ) || (userAgent.toLowerCase().indexOf('iphone') > -1 )
-  || (userAgent.toLowerCase().indexOf('ipad') > -1 ) || (userAgent.toLowerCase().indexOf('ipod') > -1 ))
-&& (userAgent.toLowerCase().indexOf('firefox') > -1)){ 
-  setInterval(function(){
-    if (window.pageYOffset > topElement && !destroyer){
-      alert('s');
-      $( '#bar1' ).css('display', 'none');
-      destroyer = 1;
+        var destroyer = 0;
+      var userAgent = window.navigator.userAgent;
+      var topElement = document.getElementById('bar2').getBoundingClientRect().top + window.pageYOffset;
+      if (((userAgent.toLowerCase().indexOf('android') > -1 ) || (userAgent.toLowerCase().indexOf('iphone') > -1 )
+      || (userAgent.toLowerCase().indexOf('ipad') > -1 ) || (userAgent.toLowerCase().indexOf('ipod') > -1 ))
+    && (userAgent.toLowerCase().indexOf('firefox') > -1)){ 
+      setInterval(function(){
+        if (window.pageYOffset > topElement && !destroyer){
+          alert('lal');
+          $( '#bar1' ).css('display', 'none');
+          destroyer = 1;
+        }
+        else 
+          if (window.pageYOffset <= topElement && destroyer){
+            $( '#bar1' ).css('display', 'block');
+            destroyer = 0;
+          }
+        // psevdo.innerHTML += $( '#bar2' ).fixedsticky('update').isFixedToTop; 
+      }, 20)  
     }
-    else 
-      if (window.pageYOffset <= topElement && destroyer){
-        $( '#bar1' ).css('display', 'block');
-        destroyer = 0;
-      }
-    // psevdo.innerHTML += $( '#bar2' ).fixedsticky('update').isFixedToTop; 
-  }, 20)  
-  }  
 })
