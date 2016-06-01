8224ffb10f0e6521ad47348702bbde4ac6057d45
Polodine <bamzurov72@gmail.com>
2016-06-01 08:34:51 +0300

    text

--- test/1.js
+++ test/1.js
@@ -1,16 +1,36 @@
 !function(){
 
-  var headers = document.querySelectorAll('.header');
-  
+  if (getComputedStyle(document.querySelector('.header').parentNode).position == "sticky")
+    return;
+
+  var headers = document.querySelectorAll('.header');  
+
   for (var i = 0; i < headers.length; i++){
-    headers[i].parentElement.style.position = 'sticky';
-    headers[i].parentElement.style.top = 0;
-    window.addEventListener('scroll', scrollListenerStickyHeader(headers[i]));
+    var currentListener = scrollListenerStickyHeader(headers[i]);
+    window.addEventListener('scroll', currentListener);
   }
+
+  setInterval(function(){
+    lal.innerHTML += window.innerHeight + " " + document.documentElement.clientHeight + " " 
+    + document.documentElement.scrollTop + " ";
+  }, 3000);
 
   function scrollListenerStickyHeader(header){
-    if (header.getBoundingClientRect().top <= 0){
 
+    var topCoordsOfStickyHeader = header.getBoundingClientRect().top + window.pageYOffset;
+    var error = 0;
+
+    return function(){
+      error = (document.documentElement.clientHeight - window.innerHeight > 0) ? 
+              (document.documentElement.clientHeight - window.innerHeight) : 0;
+      if (window.pageYOffset >= topCoordsOfStickyHeader + error){
+        header.style.position = "fixed";
+        header.style.top = 0;
+      }
+      else 
+        header.style.position = "static";
     }
+
   }
+
 }();
