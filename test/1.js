fd2b76de1c7b7ef4d51c605bb07af30d3c9736cb
softevol <softevol@softevols-iMac.local>
2016-06-02 13:43:36 +0300

    waf

--- test/1.js
+++ test/1.js
@@ -1,52 +1,52 @@
-!function(){
+// !function(){
 
-  if (getComputedStyle(document.querySelector('.header').parentNode).position == 'sticky') // For browsers, that support position: sticky
-    return;
+//   if (getComputedStyle(document.querySelector('.header').parentNode).position == 'sticky') // For browsers, that support position: sticky
+//     return;
 
-  var headers = document.querySelectorAll('.header');  
+//   var headers = document.querySelectorAll('.header');  
 
-  for (var i = 0; i < headers.length; i++){
-    var currentListener = scrollListenerStickyHeader(headers[i]);
-    window.addEventListener('scroll', currentListener);
-  }
+//   for (var i = 0; i < headers.length; i++){
+//     var currentListener = scrollListenerStickyHeader(headers[i]);
+//     window.addEventListener('scroll', currentListener);
+//   }
 
-  var error = 0;
-  setInterval(function(){
-    lal.innerHTML += window.innerHeight + ' ' + document.documentElement.clientHeight + ' ' + document.body.clientHeight + ' '
-    + document.body.scrollTop + ' ';
-  }, 1000);
+//   var error = 0;
+//   setInterval(function(){
+//     lal.innerHTML += window.innerHeight + ' ' + document.documentElement.clientHeight + ' ' + document.body.clientHeight + ' '
+//     + document.body.scrollTop + ' ';
+//   }, 1000);
 
-  window.addEventListener('resize', resizeListenerError);
+//   window.addEventListener('resize', resizeListenerError);
 
-  function scrollListenerStickyHeader(header){
+//   function scrollListenerStickyHeader(header){
 
-    var topCoordsOfStickyHeader = header.getBoundingClientRect().top + window.pageYOffset;
+//     var topCoordsOfStickyHeader = header.getBoundingClientRect().top + window.pageYOffset;
 
-    return function(){
+//     return function(){
 
-      if (window.pageYOffset >= topCoordsOfStickyHeader + error){
-        header.style.position = 'fixed';
-        header.style.top = -error + 'px';
-      }
+//       if (window.pageYOffset >= topCoordsOfStickyHeader + error){
+//         header.style.position = 'fixed';
+//         header.style.top = -error + 'px';
+//       }
       
-      else 
-        header.style.position = 'static';
-    }
+//       else 
+//         header.style.position = 'static';
+//     }
 
-  }
+//   }
 
-  function resizeListenerError(){
-    if (document.body.scrollTop < 0){
-      window.addEventListener('scroll', considerError);
-    }
-  }
+//   function resizeListenerError(){
+//     if (document.body.scrollTop < 0){
+//       window.addEventListener('scroll', considerError);
+//     }
+//   }
 
-  function considerError(){
-    error = (document.documentElement.clientHeight - window.innerHeight > 0) ? 
-            (document.documentElement.clientHeight - window.innerHeight) : 0;
-  }
+//   function considerError(){
+//     error = (document.documentElement.clientHeight - window.innerHeight > 0) ? 
+//             (document.documentElement.clientHeight - window.innerHeight) : 0;
+//   }
 
-}();
+// }();
 
   
 
